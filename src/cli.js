530fadfdea5d9659611a923a035410c618586be6
Konstantin Kolmakov <uralskiics@gmail.com>
2023-05-07 22:07:24 +0500

    add new files and update package

---
+++ src/cli.js
@@ -0,0 +1,12 @@
+import readlineSync from 'readline-sync';
+
+const questionOfName = () => {
+    console.log('Welcome to the Brain Games!');
+
+    const userName = readlineSync.question('May I have you name? ');
+    console.log('Hello, ' + userName + '!');
+    
+    return questionOfName;
+}
+
+export default questionOfName;
