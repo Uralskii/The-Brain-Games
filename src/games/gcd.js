23ca7bbe7918e970a793b4bdb9011fb6a161c315
Konstantin Kolmakov <uralskiics@gmail.com>
2023-08-23 13:25:28 +0500

    fix game gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,24 +1,26 @@
 import makeGames from '../index.js';
 import getRandomInRange from '../getRandomInRange.js';
 
-const largestDevisor = (num1, num2) => {
-  let devisor;
-  if (num1 < num2) {
-    for (let i = num1; i > 0; i -= 1) {
-      if (num1 % i === 0 && num2 % i === 0) {
-        devisor = i;
-        return devisor;
-      }
-    }
-  } else if (num1 > num2) {
-    for (let i = num2; i > 0; i -= 1) {
-      if (num1 % i === 0 && num2 % i === 0) {
-        devisor = i;
-        return devisor;
-      }
-    }
-  } return num1;
-};
+// const largestDevisor = (num1, num2) => {
+//   let devisor;
+//   if (num1 < num2) {
+//     for (let i = num1; i > 0; i -= 1) {
+//       if (num1 % i === 0 && num2 % i === 0) {
+//         devisor = i;
+//         return devisor;
+//       }
+//     }
+//   } else if (num1 > num2) {
+//     for (let i = num2; i > 0; i -= 1) {
+//       if (num1 % i === 0 && num2 % i === 0) {
+//         devisor = i;
+//         return devisor;
+//       }
+//     }
+//   } return num1;
+// };
+
+const largestDevisor = (x, y) => (y === 0 ? x : largestDevisor(y, x % y));
 
 const makeGameGcd = () => {
   const rulesOfGame = 'Find the greatest common divisor of given numbers.';
